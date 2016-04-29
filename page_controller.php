fa0cfd3e4d91549f463d518e35d5a0c41e66170f
Vishal Parashar <vishalparashar496@gmail.com>
2016-04-29 14:20:57 +0530

    Update page_controller.php

--- page_controller.php
+++ page_controller.php
@@ -1,34 +1,171 @@
 <?php
 require_once("database.php");
 
-$image_set = $database->find_all_images();
-$all_record = $database->find_all_record();
-$link_set = $database->find_all_social_link();
-$study_set = $database->find_all_study_material();
 
-for($i=0;$i<=4;$i++){
-    if($image_set[$i][1] == $_GET["record"]){
-	    $image_name = $image_set[$i][2];
+  class PagesController{
+    public function home() {
+      $database = new MySQLDatabase();
+	  $record_set = $database->find_all_record();
+      $array = array();
+      while($row = mysqli_fetch_assoc($record_set)){
+      $array[] = $row;
+      }
+	  $array_length = count($array);
+      require_once('index_languages.php');
+    }
+	public function PYTHON() {
+	  $database = new MySQLDatabase();
+      $image_set = $database->find_images('PYTHON');   // image search from database.
+	  $record_set = $database->find_all_record_discrip('PYTHON');  // Language discription search from database.
+	  $link_set = $database->find_all_social_link('PYTHON');  // Social Link search from database.
+	  $study_set = $database->find_all_study_material('PYTHON');  // Study Material search from database.
+	  $array_image = array(); // Array for image record.
+	  while($row_image = mysqli_fetch_assoc($image_set)){
+	    $array_image[] = $row_image;
 		}
-	}
-
-for($i=0;$i<=4;$i++){
-    if($all_record[$i][0] == $_GET["record"]){
-	    $record_name = $all_record[$i][1];
-		$record_discrip = $all_record[$i][2];
+	  $array_record_set = array(); // Array for record data.
+	  while($row_record_set = mysqli_fetch_assoc($record_set)){
+	    $array_record_set[] = $row_record_set;
 		}
-	}
-	
-for($i=0;$i<=19;$i++){
-    if($link_set[$i][1] == $_GET["record"]){
-	     $link[] = $link_set[$i][2];
-		 }
-	}	 
+	  $array_social_links = array(); // Array for social link data.
+      while($row_social_link = mysqli_fetch_assoc($link_set)){
+	    $array_social_links[] = $row_social_link;
+		}
+        $array_social_links_length = count($array_social_links); // Length of array for social link.
+      $array_study_set = array(); // array for study set data.
+      while($row_study_set = mysqli_fetch_assoc($study_set)){
+	    $array_study_set[] = $row_study_set;
+		}
+        $array_study_set_length = count($array_study_set);// Length of array for study set.
+		
+        $record_discrip = $array_record_set[0]['discrip'];		
+		$image_name = $array_image[0]['image_name'];
+		$record_name = 'PYTHON';
+      require_once('languages.php');
+    }
+	public function JAVA() {
+	  $database = new MySQLDatabase();
+      $image_set = $database->find_images('JAVA'); // image search from database.
+	  $record_set = $database->find_all_record_discrip('JAVA'); // Language discription search from database.
+	  $link_set = $database->find_all_social_link('JAVA'); // Social Link search from database.
+	  $study_set = $database->find_all_study_material('JAVA'); // Study Material search from database.
+	  $array_image = array();
+	  while($row_image = mysqli_fetch_assoc($image_set)){
+	    $array_image[] = $row_image;
+		}
+	  $array_record_set = array();
+	  while($row_record_set = mysqli_fetch_assoc($record_set)){
+	    $array_record_set[] = $row_record_set;
+		}
+	  $array_social_links = array();
+      while($row_social_link = mysqli_fetch_assoc($link_set)){
+	    $array_social_links[] = $row_social_link;
+		}
+        $array_social_links_length = count($array_social_links);
+      $array_study_set = array();
+      while($row_study_set = mysqli_fetch_assoc($study_set)){
+	    $array_study_set[] = $row_study_set;
+		}
+        $array_study_set_length = count($array_study_set);
+		
+        $record_discrip = $array_record_set[0]['discrip'];		
+		$image_name = $array_image[0]['image_name'];
+		$record_name = 'JAVA';
+      require_once('languages.php');
+    }
+	public function C() {
+	  $database = new MySQLDatabase();
+      $image_set = $database->find_images('C'); // image search from database.
+	  $record_set = $database->find_all_record_discrip('C'); // Language discription search from database.
+	  $link_set = $database->find_all_social_link('C'); // Social Link search from database.
+	  $study_set = $database->find_all_study_material('C'); // Study Material search from database.
+	  $array_image = array();
+	  while($row_image = mysqli_fetch_assoc($image_set)){
+	    $array_image[] = $row_image;
+		}
+	  $array_record_set = array();
+	  while($row_record_set = mysqli_fetch_assoc($record_set)){
+	    $array_record_set[] = $row_record_set;
+		}
+	  $array_social_links = array();
+      while($row_social_link = mysqli_fetch_assoc($link_set)){
+	    $array_social_links[] = $row_social_link;
+		}
+        $array_social_links_length = count($array_social_links);
+      $array_study_set = array();
+      while($row_study_set = mysqli_fetch_assoc($study_set)){
+	    $array_study_set[] = $row_study_set;
+		}
+        $array_study_set_length = count($array_study_set);
+		
+        $record_discrip = $array_record_set[0]['discrip'];		
+		$image_name = $array_image[0]['image_name'];
+		$record_name = 'C';
+      require_once('languages.php');
+    }
+	public function LISP() {
+	  $database = new MySQLDatabase();
+      $image_set = $database->find_images('LISP'); // image search from database.
+	  $record_set = $database->find_all_record_discrip('LISP'); // Language discription search from database.
+	  $link_set = $database->find_all_social_link('LISP'); // Social Link search from database.
+	  $study_set = $database->find_all_study_material('LISP'); // Study Material search from database.
+	  $array_image = array();
+	  while($row_image = mysqli_fetch_assoc($image_set)){
+	    $array_image[] = $row_image;
+		}
+	  $array_record_set = array();
+	  while($row_record_set = mysqli_fetch_assoc($record_set)){
+	    $array_record_set[] = $row_record_set;
+		}
+	  $array_social_links = array();
+      while($row_social_link = mysqli_fetch_assoc($link_set)){
+	    $array_social_links[] = $row_social_link;
+		}
+        $array_social_links_length = count($array_social_links);
+      $array_study_set = array();
+      while($row_study_set = mysqli_fetch_assoc($study_set)){
+	    $array_study_set[] = $row_study_set;
+		}
+        $array_study_set_length = count($array_study_set);
+		
+        $record_discrip = $array_record_set[0]['discrip'];		
+		$image_name = $array_image[0]['image_name'];
+		$record_name = 'LISP';
+      require_once('languages.php');
+    }
+	public function PERL() {
+$database = new MySQLDatabase();
+      $image_set = $database->find_images('PERL'); // image search from database.
+	  $record_set = $database->find_all_record_discrip('PERL'); // Language discription search from database.
+	  $link_set = $database->find_all_social_link('PERL'); // Social Link search from database.
+	  $study_set = $database->find_all_study_material('PERL'); // Study Material search from database.
+	  $array_image = array();
+	  while($row_image = mysqli_fetch_assoc($image_set)){
+	    $array_image[] = $row_image;
+		}
+	  $array_record_set = array();
+	  while($row_record_set = mysqli_fetch_assoc($record_set)){
+	    $array_record_set[] = $row_record_set;
+		}
+	  $array_social_links = array();
+      while($row_social_link = mysqli_fetch_assoc($link_set)){
+	    $array_social_links[] = $row_social_link;
+		}
+        $array_social_links_length = count($array_social_links);
+      $array_study_set = array();
+      while($row_study_set = mysqli_fetch_assoc($study_set)){
+	    $array_study_set[] = $row_study_set;
+		}
+        $array_study_set_length = count($array_study_set);
+		
+        $record_discrip = $array_record_set[0]['discrip'];		
+		$image_name = $array_image[0]['image_name'];
+		$record_name = 'PERL';
+      require_once('languages.php');
+    }
 
-for($i=0;$i<=9;$i++){
-    if($study_set[$i][1] == $_GET["record"]){
-	     $study[] = $study_set[$i][2];
-		 }
-	}	
-include("languages.php");	
+    public function error() {
+      require_once('views/pages/error.php');
+    }
+  }
 ?>
